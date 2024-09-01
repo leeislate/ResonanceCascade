6d1ac7d105ad82e4f007a4a840b59305b1b64c63
leeislate <134997564+leeislate@users.noreply.github.com>
2024-08-31 20:06:55 -0700

    Important, utterly essential changes made!

--- src/main/java/net/sloth/resonancecascade/ResonanceCascade.java
+++ src/main/java/net/sloth/resonancecascade/ResonanceCascade.java
@@ -58,7 +58,7 @@ public class ResonanceCascade {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event)
         {
-
+            /* Ratatouille */
         }
     }
 }
